e214dc51711ef63629bd93e0d9192fe51c033f9d
yuviduyasasvin <yasasvinyuvidu@gmail.com>
2026-03-18 12:39:50 +0530

    Update vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -7,7 +7,6 @@ export default defineConfig({
   test: {
     globals: true,
     environment: 'jsdom',
-    setupFiles: './src/setupTests.js',
     coverage: {
       provider: 'v8',
       reporter: ['text', 'json', 'html', 'lcov'],
