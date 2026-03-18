01e6b20b9b71601491194db7b281cae6c7c5ad46
yuviduyasasvin <yasasvinyuvidu@gmail.com>
2026-03-18 12:17:05 +0530

    add testing

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './src/setupTests.js',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      include: ['src/**/*.{js,jsx}'],
+      exclude: ['node_modules/**', 'src/main.jsx', 'src/App.jsx'],
+    },
+  },
 })
