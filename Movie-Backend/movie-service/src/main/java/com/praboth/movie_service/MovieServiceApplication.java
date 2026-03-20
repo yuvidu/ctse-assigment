ba65fce74aa0e9a62833a6654e7a7dacce9b2bd6
Praboth Sandaruwan <it22148254@my.sliit.lk>
2026-03-20 09:01:18 +0530

    feat: Implement the Movie Service with REST API, MongoDB persistence, and Docker support.

---
+++ Movie-Backend/movie-service/src/main/java/com/praboth/movie_service/MovieServiceApplication.java
@@ -0,0 +1,13 @@
+package com.praboth.movie_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieServiceApplication.class, args);
+	}
+
+}
